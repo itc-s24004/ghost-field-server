c84770b65930a41b3bdf1344d05389bfc1be35d7
s24004 <s24004@std.it-college.ac.jp>
2026-02-19 17:07:54 +0900

    テスト用ルームを追加

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,131 @@
 import { app, io, server } from "./server/server.js";
-import { GhostFieldServer, GhostField_Element } from "ghost-field"
+import { GhostField_Server, GhostFieldCore } from "ghost-field"
 
-import type { GhostField_CardID } from "ghost-field";
+import cards from "../cards.json" with { type: "json" };
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT);
+server.listen(PORT,() =>  {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
 
-GhostFieldServer.listen(app, io);
 
-
-type custom = {
-    image: string;
+type RoomData = {
+    id: string;
+    server: GhostField_Server;
+    isPublic: boolean;
 }
 
 
-new GhostFieldServer<custom>(
-    {
-        "card": {
-            data: [
-                {
-                    format: 0,
-                    id: "test-card-001" as GhostField_CardID,
-                    name: "",
-                    description: "",
-                    price: 0,
-                    element: GhostField_Element.Normal,
-                    canDrop: false,
-                    tags: [],
-                    custom: {
-                        image: ""
-                    }
-                }
-            ],
-            table: {
-                ["test-card-001" as GhostField_CardID]: 3
-            }
-        },
-        ghost: {
 
-        }
-        
+const serverRooms: RoomData[] = [];
+
+
+function createRoom(id: string | undefined, data: GhostFieldCore.GF_Initial_Game, devRoom: boolean = false) {
+    let isPublic = devRoom;
+    if (!id) {
+        id = crypto.randomUUID();
+        isPublic = true;
     }
-);
+    
+    if (serverRooms.find(r => r.id === id)) {
+        throw new Error("そのIDの部屋は既に存在しています");
+    }
+    if (!/^[a-zA-Z0-9-]{1,36}$/.test(id)) {
+        throw new Error("部屋IDは英数字とハイフンのみの36文字以下である必要があります");
+    }
+    const socket = io.of(`/${id}`);
+    
+    const server = new GhostField_Server(socket, data, {
+        autoClose: !devRoom,
+        timeout: devRoom ? undefined : 1000 * 60, // 60秒
+        onClose: () =>  {
+            const index = serverRooms.findIndex(r => r.id === id);
+            if (index !== -1) {
+                serverRooms.splice(index, 1);
+            }
+            io._nsps.delete(`/${id}`);
+            console.log(`ルーム ${id} が閉じられました`);
+        }
+    });
+    console.log("ルームが作成されました: ", id);
+
+    serverRooms.push({
+        id,
+        server,
+        isPublic
+    });
+
+    return id;
+}
+
+
+
+type RoomList = {
+    id: string;
+    connection: number;
+}
+
+function getRoomList(): RoomList[] {
+    return serverRooms.reduce((list, room) => {
+        if (room.isPublic) {
+            list.push({
+                id: room.id,
+                connection: room.server.socketCount
+            });
+        }
+        return list;
+    }, [] as RoomList[]);
+}
+
+
+
+
+type RoomInfo = GhostFieldCore.GF_Initial_Game
+
+function getRoom(id: string): RoomInfo | null {
+    const room = serverRooms.find(r => r.id === id);
+    if (!room) return null;
+    return room.server.game.initData;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("/room", (req, res) => {
+    console.log("get room list");
+    res.json(getRoomList());
+
+}).get("/room/:id", (req, res) => {
+    const id = req.params.id;
+    const room = getRoom(id);
+
+    if (!room) return res.status(404).json({});;
+    res.json(room);
+
+}).post("/room", (req, res) => {
+    const { id, data } = req.body;
+    try {
+        const roomId = createRoom(id, data);
+        res.json({ id: roomId });
+
+    } catch (e) {
+        res.status(400).json({ error: e instanceof Error ? e.message : "不明なエラー" });
+
+    }
+
+});
+
+createRoom("dev", {cards: cards as unknown as GhostFieldCore.GF_CardComponent[], meta: {}}, true);
+
--- src/server/server.ts
+++ src/server/server.ts
@@ -10,6 +10,24 @@ import { Server as IO_Server} from "socket.io";
 
 //!!! pnpmだと型推論でエラーが出るため明示的に型を指定する npmだと発生しない
 export const app: express.Express = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, access_token'
+    )
+
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+        res.send(200)
+    } else {
+        next()
+    }
+})
+
+
 app
 .use(express.urlencoded({ extended: true }))
 .use(express.json());
@@ -18,9 +36,9 @@ app
 export const server = http.createServer(app);
 export const io = new IO_Server(
     {
-        cors: {
-            origin: "*",
-            methods: ["GET", "POST"]
-        }
+        connectionStateRecovery: {// 切断からの復帰を有効化
+            maxDisconnectionDuration: 60 * 1000
+        },
+        transports: ["websocket"]
     }
 ).listen(server);
